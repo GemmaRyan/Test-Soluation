d4dd9d9eb7823fdef31a6d9789b698cfe880921d
GemmaRyan-STUDENT <GemmaRyan-STUDENT@LAB-B2311-13>
2024-02-19 09:47:46 +0000

    Add project files.

---
+++ Test-Soluation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_Soluation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
